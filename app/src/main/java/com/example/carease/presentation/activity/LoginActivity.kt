8aedeb72d84c64cc33f0359f66d9dacc4d7d16a7
Karan Jadaun <73349879+KaranJadaun@users.noreply.github.com>
2023-08-25 12:37:27 +0530

    Login Backend Done!

--- app/src/main/java/com/example/carease/presentation/activity/LoginActivity.kt
+++ app/src/main/java/com/example/carease/presentation/activity/LoginActivity.kt
@@ -1,25 +1,83 @@
 package com.example.carease.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.carease.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     lateinit var signInEmail: String
     lateinit var signInPassword: String
     lateinit var signInBtn: Button
     lateinit var emailEt: EditText
     lateinit var passEt: EditText
-    lateinit var loadingDialog: LoadingDialog
     lateinit var emailError: TextView
     lateinit var passwordError: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+        val signUpTv = findViewById<TextView>(R.id.signUpTv)
+        signInBtn = findViewById(R.id.loginBtn)
+        emailEt = findViewById(R.id.emailEt)
+        passEt = findViewById(R.id.PassEt)
+        emailError = findViewById(R.id.emailError)
+        passwordError = findViewById(R.id.passwordError)
+
+        signUpTv.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        signInBtn.setOnClickListener {
+            performLogin()
+        }
+    }
+    private fun performLogin() {
+        val email = emailEt.text.toString()
+        val password = passEt.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this,
+                "Please fill all fields",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication Successful",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            .addOnCanceledListener {
+                Toast.makeText(this, "Error Occurred", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/carease/presentation/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/carease/presentation/activity/SignUpActivity.kt
@@ -1,12 +1,79 @@
 package com.example.carease.presentation.activity
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.carease.R
+import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var fullName: EditText
+    private lateinit var emailEt: EditText
+    private lateinit var passEt: EditText
+    private lateinit var CpassEt: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth = Firebase.auth
+        val signUpBtn = findViewById<Button>(R.id.signUpBtn_signUpPage)
+        fullName = findViewById(R.id.nameEt_signUpPage)
+        emailEt = findViewById(R.id.emailEt_signUpPage)
+        passEt = findViewById(R.id.PassEt_signUpPage)
+        CpassEt = findViewById(R.id.cPassEt_signUpPage)
+        val signInTv = findViewById<TextView>(R.id.signInTv_signUpPage)
+
+        signInTv.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        signUpBtn.setOnClickListener {
+            performSignUp()
+        }
+
+    }
+    private fun performSignUp() {
+        val email = emailEt.text.toString()
+        val password = passEt.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication Successful",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            .addOnCanceledListener {
+                Toast.makeText(this, "Error Occurred", Toast.LENGTH_SHORT).show()
+            }
     }
 }
