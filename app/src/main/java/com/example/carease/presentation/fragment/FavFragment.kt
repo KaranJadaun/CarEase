6964370cbde131b0f02d0d30b359457f57c17002
Karan Jadaun <73349879+KaranJadaun@users.noreply.github.com>
2023-09-17 09:41:04 +0530

    Shopping Page backend

--- app/src/main/java/com/example/carease/presentation/fragment/FavFragment.kt
+++ app/src/main/java/com/example/carease/presentation/fragment/FavFragment.kt
@@ -5,16 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
 import com.example.carease.R
 
-class FavFragment: Fragment() {
+class FavFragment : Fragment() {
+
+    lateinit var animationView: LottieAnimationView
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_fav, container, false)
-    }
+        val view =  inflater.inflate(R.layout.fragment_fav, container, false)
 
+        animationView = view.findViewById(R.id.animationViewFavPage)
+
+        animationView.playAnimation()
+        animationView.loop(true)
+
+        return view
+    }
 }
